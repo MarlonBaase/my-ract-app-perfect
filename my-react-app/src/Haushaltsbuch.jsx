6d17ce240263838f81cb7c4d207bf99e2eb4dd5a
MarlonBaase <baase.marlon28@gmail.com>
2026-06-19 19:29:22 +0200

    Version 1.42 - 2026-06-19 19:29:21

--- my-react-app/src/Haushaltsbuch.jsx
+++ my-react-app/src/Haushaltsbuch.jsx
@@ -188,38 +188,6 @@ export default function haushaltsbuch() {
     ladeAlles()
   }
 
-
-
-
-const pruefeWiederkehrende = async (liste) => {
-  console.log("Liste:", liste)  // ← Was kommt an?
-  const { data: { user } } = await supabase.auth.getUser()
-  const heute = new Date().toISOString().split("T")[0]
-  console.log("Heute:", heute)  // ← Welches Datum?
-
-  for (const eintrag of liste) {
-    console.log("Eintrag:", eintrag.naechste_faelligkeit, "<=", heute, "?", eintrag.naechste_faelligkeit <= heute)
-    
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   const ladeWiederkehrende = async () => {
     const { data: { user } } = await supabase.auth.getUser()
     const { data } = await supabase
