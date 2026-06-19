162fb375414c029be32ae48ace2a3b31ca94acc8
MarlonBaase <baase.marlon28@gmail.com>
2026-06-19 17:03:11 +0200

    Version 1.33 - 2026-06-19 17:03:10

--- my-react-app/src/Assetklassen.jsx
+++ my-react-app/src/Assetklassen.jsx
@@ -8,16 +8,10 @@ export default function Assetklassen() {
       <h1>Products Page</h1>
     <BrowserRouter>
       <nav>
-        <Link to="/LF">Liquidität & Forderungen</Link> |{" "}
-        <Link to="/Assetklassen/car">Wertpapiere & Derivate</Link> |{" "}
-        <Link to="/Assetklassen/car">Immobilien</Link> |{" "}
-        <Link to="/Assetklassen/car">Krypto & Web3</Link> |{" "}
-        <Link to="/Assetklassen/car">Sachwerte & Edelmetalle</Link> |{" "}
-        <Link to="/Assetklassen/car">Geschäftliches & Immaterielles</Link> |{" "}
-        <Link to="/Assetklassen/car">Versicherungen, Vorsorge & Verbindlichkeiten</Link> 
+        <Link to="/lf">Liquidität & Forderungen</Link> |{" "}
       </nav>
       <Routes>
-        <Route path="/LF" element={<LF />} />
+        <Route path="/lf" element={<LF />} />
       </Routes>
     </BrowserRouter>
     </div>
