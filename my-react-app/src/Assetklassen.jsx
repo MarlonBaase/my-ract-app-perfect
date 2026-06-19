c06682afa7e915f9021243d6c73f2d432fda811a
MarlonBaase <baase.marlon28@gmail.com>
2026-06-19 19:28:33 +0200

    Version 1.41 - 2026-06-19 19:28:32

--- my-react-app/src/Assetklassen.jsx
+++ my-react-app/src/Assetklassen.jsx
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
+import { StrictMode, useEffect, useState } from 'react'
+import { supabase } from './supabase'
 import lf from './lf'
 
 export default function Assetklassen() {
@@ -8,7 +10,7 @@ export default function Assetklassen() {
       <h1>Products Page</h1>
       <BrowserRouter>
         <nav>
-          <Link to="/assetklassen/lf">Liquidität & Forderungen</Link> |{" "}
+          <Link to="/lf">Liquidität & Forderungen</Link> |{" "}
         </nav>
         <Routes>
           <Route path='lf' element={<lf />} />
