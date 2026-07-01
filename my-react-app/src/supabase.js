af929e8dbcf85f68629679c592561b5666fbd90c
MarlonBaase <baase.marlon28@gmail.com>
2026-07-01 19:38:15 +0200

    Version 1.135 - 2026-07-01 19:38:15

--- my-react-app/src/supabase.js
+++ my-react-app/src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://zfcpkpmtrsqiuxvkhtbd.supabase.co',
-  'sb_publishable_7imHzM_rNc_6B0NsVD1Wog_Uc0vI7Jx'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmY3BrcG10cnNxaXV4dmtodGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODUwODYsImV4cCI6MjA5NjM2MTA4Nn0.ZjPnnx_ubQwJGS32PMi5hmkjoHmOopjBG38prWIuJHk'
 )
