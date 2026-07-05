66951f08e2c5b07f7c1ec8f977f17252ce09a31a
MarlonBaase <baase.marlon28@gmail.com>
2026-07-05 14:47:58 +0200

    Version 1.157 - 2026-07-05 14:47:58

--- my-react-app/src/Assetklassen.jsx
+++ my-react-app/src/Assetklassen.jsx
@@ -1,20 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link, Navigate, Outlet } from 'react-router-dom'
-import { StrictMode, useEffect, useState } from 'react'
-import { supabase } from './supabase'
-import Lf from './Lf.jsx'
-import Wd from './Wd.jsx'
-import AssetSidebar from './AssetSidebar.jsx'
-import './index.css'
+import { Outlet } from 'react-router-dom'
+import AssetSidebar from './AssetSidebar'
 
-export default function Assetklassen() {
-    return (
+export default function Assetklassen({ darkMode }) {
+  return (
     <div style={{ display: "flex" }}>
       <AssetSidebar darkMode={darkMode} />
       <div style={{ flex: 1, overflowY: "auto" }}>
         <Outlet />
       </div>
     </div>
-
   )
 }
