dd00bd6ab84848ceb46fd1e271a1bf6555fef40a
MarlonBaase <baase.marlon28@gmail.com>
2026-07-05 13:53:09 +0200

    Version 1.149 - 2026-07-05 13:53:09

--- my-react-app/src/assetklassen/Girokonto.jsx
+++ my-react-app/src/assetklassen/Girokonto.jsx
@@ -23,8 +23,7 @@ export default function Girokonto() {
         const { data } = await supabase
             .from("girokonto")
             .select("*")
-            .eq("user_id", user.id)
-            .eq("typ", "girokonto")  // ← nur Girokonten
+            .eq("benutzer_id", user.id)
             .order("name", { ascending: true })
 
         if (data) setListeGirokonto(data)
@@ -41,12 +40,9 @@ export default function Girokonto() {
         if (!name || !bank || !iban || !wert || !einlage_summe || !waehrung || !eroeffnet_am) return
         const { data: { user } } = await supabase.auth.getUser()
         await supabase.from("girokonto").insert({
-            user_id: user.id,
-            typ: "girokonto",
-            name: name,
-            bank: bank,
+            benutzer_id: user.id,
+            name_der_bank: bank,
             iban: iban,
-            wert: parseFloat(wert) || 0,
             einlage_summe: parseFloat(einlage_summe) || 0,
             waehrung: waehrung,
             eroeffnet_am: eroeffnet_am
@@ -67,11 +63,10 @@ export default function Girokonto() {
 
     const bearbeitenOeffnen = (eintrag) => {
         setZuBearbeiten(eintrag)
-        setName(eintrag.name)
-        setBank(eintrag.bank)
+        setName(eintrag.name_der_bank)
+        setBank(eintrag.name_der_bank)
         setIban(eintrag.iban)
-        setWert(eintrag.wert)
-        setSumme(eintrag.einlage_summe)
+        setWert(eintrag.einlage_summe)
         setWaehrung(eintrag.waehrung)
         setEroeffnet_am(eintrag.eroeffnet_am)
         setModalOffen(true)
@@ -85,11 +80,8 @@ export default function Girokonto() {
     const girokontoSpeichern = async () => {
 
         await supabase.from("girokonto").update({
-            typ: "girokonto",
-            name: name,
-            bank: bank,
+            name_der_bank: bank,
             iban: iban,
-            wert: parseFloat(wert) || 0,
             einlage_summe: parseFloat(einlage_summe) || 0,
             waehrung: waehrung,
             eroeffnet_am: eroeffnet_am
@@ -108,9 +100,9 @@ export default function Girokonto() {
             <ul>
                 {listeGirokonto.map((e) => (
                     <li key={e.id}>
-                        {e.name} | {e.bank} |  {e.iban} | {e.wert} | {e.einlage_summe} | {e.waehrung} | {e.eroeffnet_am}
+                        {e.name_der_bank} | {e.iban} | {e.einlage_summe} | {e.waehrung} | {e.eroeffnet_am}
                         <button onClick={() => bearbeitenOeffnen(e)}>✏️</button>
-                        <button onClick={() => eintragLoeschen(e.id, e.typ)}>🗑️</button>
+                        <button onClick={() => eintragLoeschen(e.id)}>🗑️</button>
                     </li>
                 ))}
             </ul>
