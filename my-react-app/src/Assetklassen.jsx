9240c61ccf2fe141017b9e91b16e85b44b4b44cb
MarlonBaase <baase.marlon28@gmail.com>
2026-07-05 14:50:53 +0200

    Version 1.158 - 2026-07-05 14:50:53

--- my-react-app/src/Assetklassen.jsx
+++ my-react-app/src/Assetklassen.jsx
@@ -1,10 +1,38 @@
 import { Outlet } from 'react-router-dom'
 import AssetSidebar from './AssetSidebar'
+import { useState } from 'react'
 
 export default function Assetklassen({ darkMode }) {
+  const [sidebarOffen, setSidebarOffen] = useState(true)
+
   return (
     <div style={{ display: "flex" }}>
-      <AssetSidebar darkMode={darkMode} />
+      
+      {/* Toggle Button */}
+      <button
+        onClick={() => setSidebarOffen(!sidebarOffen)}
+        style={{
+          position: "fixed",
+          top: "70px",
+          left: sidebarOffen ? "248px" : "0px",
+          zIndex: 999,
+          backgroundColor: "#4F6EF7",
+          color: "white",
+          border: "none",
+          borderRadius: "0 8px 8px 0",
+          padding: "0.5rem 0.4rem",
+          cursor: "pointer",
+          fontSize: "0.75rem",
+          transition: "left 0.2s",
+        }}
+      >
+        {sidebarOffen ? "◀" : "▶"}
+      </button>
+
+      {/* Sidebar */}
+      {sidebarOffen && <AssetSidebar darkMode={darkMode} />}
+
+      {/* Inhalt */}
       <div style={{ flex: 1, overflowY: "auto" }}>
         <Outlet />
       </div>
