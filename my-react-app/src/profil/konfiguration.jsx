0a11f4e2cf66b765391467dd4e4a16fb08d728eb
MarlonBaase <baase.marlon28@gmail.com>
2026-07-12 15:29:00 +0200

    Version 1.177 - 2026-07-12 15:29:00

--- my-react-app/src/profil/konfiguration.jsx
+++ my-react-app/src/profil/konfiguration.jsx
@@ -1,3 +1,82 @@
-export default function Konfiguration() {
-  return <h2>Konfiguration</h2>;
+import { supabase } from "./supabase";
+import { useEffect, useState } from "react";
+
+
+
+export default function Konfiguration({ darkMode, setDarkMode }) {
+
+  const [kategorien, setKategorien] = useState([])
+  const [neueKategorie, setNeueKategorie] = useState("")
+  const [kategorieInter, setkategorieInter] = useState("");
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await ladeKategorien()
+      } catch (err) {
+        console.error("Fehler in init:", err)
+      }
+    }
+    init()
+  }, [])
+
+  const ladeKategorien = async () => {
+      const { data } = await supabase
+        .from("transaktionskategorie")
+        .select("*")
+        .order("name", { ascending: true })
+  
+      if (data) setKategorien(data)
+    }
+
+    const kategorieHinzufuegen = async () => {
+        if (!neueKategorie) return
+        const { data: { user } } = await supabase.auth.getUser()
+        await supabase.from("transaktionskategorie").insert({
+          benutzer_id: user.id,
+          name: neueKategorie,
+          ist_vordefiniert: false,
+          erstellt_am: new Date()
+        })
+        setNeueKategorie("")
+        ladeKategorien()
+      }
+
+      const kategorieLoeschen = async (id, ist_vordefiniert) => {
+          if (ist_vordefiniert === false) {
+            await supabase.from("transaktionskategorie").delete().eq("id", id)
+          }
+          ladeKategorien()
+        }
+
+  return (
+    <div>
+    <button onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+
+      <div>
+        <h4>Eigene Kategorie hinzufügen</h4>
+        <input
+          value={neueKategorie}
+          onChange={(e) => setNeueKategorie(e.target.value)}
+          placeholder="z.B. 🎮 Gaming"
+        />
+        <button onClick={kategorieHinzufuegen}>Kategorie hinzufügen</button>
+      </div>
+    
+     <ul>
+        {kategorien.map((e) => (
+          <li key={e.id}>
+            {e.name}
+            {!e.ist_vordefiniert && (
+              <button onClick={() => kategorieLoeschen(e.id, e.ist_vordefiniert)}>🗑️</button>
+            )}
+          </li>
+        ))}
+      </ul>
+
+      </div>
+
+  );
 }
