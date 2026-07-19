266fe1c3814d666cc74972725f3f78fb9ee7b840
MarlonBaase <baase.marlon28@gmail.com>
2026-07-19 12:31:30 +0200

    Version 1.278 - 2026-07-19 12:31:30

--- my-react-app/src/assetklassen/Girokonto.jsx
+++ my-react-app/src/assetklassen/Girokonto.jsx
@@ -25,7 +25,7 @@ export default function Girokonto() {
                 )
             `)
             .eq("asset.benutzer_id", user.id)
-            .order("asset.asset_name", { ascending: true }) // Sortierung angepasst auf asset_name, falls name_der_bank nicht indiziert ist
+            .order("asset_name", { ascending: true }) // Sortierung angepasst auf asset_name, falls name_der_bank nicht indiziert ist
 
         if (error) {
             console.error("Fehler beim Laden:", error.message)
