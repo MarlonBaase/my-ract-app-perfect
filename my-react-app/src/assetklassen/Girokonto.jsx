4df21eb91fbde61924e7db66b9a8ae6e852fbc80
MarlonBaase <baase.marlon28@gmail.com>
2026-07-22 18:04:34 +0200

    Version 1.368 - 2026-07-22 18:04:34

--- my-react-app/src/assetklassen/Girokonto.jsx
+++ my-react-app/src/assetklassen/Girokonto.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabase";
 import { handleApiError } from "../utils/errorHandler";
-import { SettingsContext } from './SettingsContext';
+import { SettingsContext } from '../SettingsContext';
 
 export default function Girokonto() {
     const [listeGirokonto, setListeGirokonto] = useState([])
