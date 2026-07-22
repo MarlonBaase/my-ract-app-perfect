3b9e715c46c28e504047376a14a5974430b656f1
MarlonBaase <baase.marlon28@gmail.com>
2026-07-22 18:07:36 +0200

    Version 1.370 - 2026-07-22 18:07:36

--- my-react-app/src/assetklassen/Girokonto.jsx
+++ my-react-app/src/assetklassen/Girokonto.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { supabase } from "../supabase";
 import { handleApiError } from "../utils/errorHandler";
 import { SettingsContext } from '../SettingsContext';
