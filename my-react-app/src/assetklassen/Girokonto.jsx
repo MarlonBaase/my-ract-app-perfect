4e6c4287731caf2dcf0bf10a468ae451aa247a5c
MarlonBaase <baase.marlon28@gmail.com>
2026-07-23 08:39:03 +0200

    Version 1.381 - 2026-07-23 08:39:03

--- my-react-app/src/assetklassen/Girokonto.jsx
+++ my-react-app/src/assetklassen/Girokonto.jsx
@@ -315,7 +315,7 @@ export default function Girokonto() {
                                 <div className="card-body">
                                     
                                     <div className="amount">
-                                        {listeTransaktionenGirokonto}
+                                        {listeTransaktionenGirokonto.betrag}
                                         {wert.toFixed(2)}
                                         {e.einzahlung_bei_eroeffnung} {e.waehrung}
                                     </div>
