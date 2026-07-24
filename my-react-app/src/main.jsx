f5609699034acf844984573ea9ca2cdb077003db
MarlonBaase <baase.marlon28@gmail.com>
2026-07-24 07:42:43 +0200

    Version 1.424 - 2026-07-24 07:42:43

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -80,7 +80,7 @@ function App() {
             <Route path="/haushaltsbuch" element={<Haushaltsbuch darkMode={darkMode} />} />
             <Route path="/profil" element={<Profil darkMode={darkMode} />}>
               <Route path="konfiguration" element={<Konfiguration darkMode={darkMode} setDarkMode={setDarkMode} />} />
-              <Route path="/zeiterfassung" element={<Zeiterfassung darkMode={darkMode} setDarkMode={setDarkMode} />} />
+              <Route path="zeiterfassung" element={<Zeiterfassung darkMode={darkMode} setDarkMode={setDarkMode} />} />
             </Route>
             <Route path="/simulation" element={<Simulation darkMode={darkMode} />} />
             <Route path="/support" element={<Support darkMode={darkMode} />} />
