3afb461533c8df4df7992567ab0b3af091d04b7d
MarlonBaase <baase.marlon28@gmail.com>
2026-08-02 14:41:16 +0200

    Version 1.507 - 2026-08-02 14:41:16

--- my-react-app/src/assetklassen/Tagesgeld.jsx
+++ my-react-app/src/assetklassen/Tagesgeld.jsx
@@ -326,7 +326,7 @@ export default function Tagesgeld() {
                 tagesgeldkonto!left(*),
                 girokonto!left(*)`)
             .eq("benutzer_id", user.id)
-            .or('tagesgeld_ist_referenzkonto.eq.true,girokonto_ist_referenzkonto.eq.true')
+            .or('tagesgeldkonto_ist_referenzkonto.eq.true,girokonto_ist_referenzkonto.eq.true')
             .order('asset_name', { ascending: true });
 
         if (data) setListeReferenzkonto(data);
