f27709463c52c9790ffae0e4cbdcecd85f139ffc
MarlonBaase <baase.marlon28@gmail.com>
2026-08-08 12:09:49 +0200

    Version 1.603 - 2026-08-08 12:09:49

--- my-react-app/src/Lf.jsx
+++ my-react-app/src/Lf.jsx
@@ -4,14 +4,6 @@ export default function Lf() {
   return (
     <div>
       <h2>Liquidität & Forderungen</h2>
-      <nav>
-        <Link to="lf_Dashboard">Dashboard</Link>
-        <Link to="girokonto">Girokonto</Link>
-        <Link to="tagesgeld">Tagesgeldkonto</Link>
-        <Link to="festgeld">Festgeld / Sparbrief</Link>
-        <Link to="geldmarktfonds">Geldmarktfonds</Link>
-        <Link to="fremdwaehrungskonto">Fremdwährungskonto</Link>
-      </nav>
       <Outlet />
     </div>
   )
