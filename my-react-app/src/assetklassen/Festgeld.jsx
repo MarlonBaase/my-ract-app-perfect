8f33ea7d0d4823fdcc01006d7eb72a517a53d7de
MarlonBaase <baase.marlon28@gmail.com>
2026-08-15 21:16:41 +0200

    Version 1.673 - 2026-08-15 21:16:41

--- my-react-app/src/assetklassen/Festgeld.jsx
+++ my-react-app/src/assetklassen/Festgeld.jsx
@@ -158,7 +158,6 @@ export default function Festgeld() {
     };
 
     const handleFestgeldSpeichern = () => {
-        console.log("Test")
         if (validateForm()) {
             console.log("Test3")
 
