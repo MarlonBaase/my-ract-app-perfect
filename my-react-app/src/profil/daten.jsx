53976e3e2f16f18b5547d52b14fcd90d78dacb72
MarlonBaase <baase.marlon28@gmail.com>
2026-08-16 12:02:17 +0200

    Version 1.700 - 2026-08-16 12:02:17

--- my-react-app/src/profil/daten.jsx
+++ my-react-app/src/profil/daten.jsx
@@ -1,16 +1,15 @@
-import { Outlet, Link } from 'react-router-dom'
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { supabase } from "../supabase";
 
-export default function daten() {
-
+export default function Daten() {
     const [inhalt, setInhalt] = useState("");
 
     useEffect(() => {
-        fetch("/version.txt")
-            .then((res) => res.text())
-            .then((text) => setInhalt(text))
-            .catch((err) => console.error("Fehler:", err));
-    }, []);
+    fetch("/daten.txt")
+      .then((res) => res.text())
+      .then((text) => setInhalt(text))
+      .catch((err) => console.error("Fehler:", err));
+  }, []);
 
-    return <div><pre>{inhalt}</pre></div>;
+  return <div><pre>{inhalt}</pre></div>;
 }
