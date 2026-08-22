dbb316219b36bf573997b5737ab64fe5e34b6cb3
MarlonBaase <baase.marlon28@gmail.com>
2026-08-22 14:40:31 +0200

    Version 1.733 - 2026-08-22 14:40:31

--- my-react-app/src/assetklassen/Fremdwaehrung.jsx
+++ my-react-app/src/assetklassen/Fremdwaehrung.jsx
@@ -1,6 +1,5 @@
 import { Outlet, Link } from 'react-router-dom'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Navbar from './Navbar'
 
 export default function Fremdwaehrung() {
     const [darkMode, setDarkMode] = useState(false)
@@ -10,7 +9,6 @@ export default function Fremdwaehrung() {
         <div>
             <h2>Fremdwaehrung</h2>
             <BrowserRouter>
-                <Navbar darkMode={darkMode} />
                 <Routes>
                     <Route path="fremdwaehrung_Stammdaten" element={<fremdwaehrung_Stammdaten darkMode={darkMode} />} />
                     <Route path="fremdwaehrung_Konto" element={<fremdwaehrung_Konto darkMode={darkMode} />} />
