6fa625da992dcbe9cdfb8468e9d530b8000fbbec
MarlonBaase <baase.marlon28@gmail.com>
2026-08-22 14:54:41 +0200

    Version 1.739 - 2026-08-22 14:54:41

--- my-react-app/src/assetklassen/Fremdwaehrung.jsx
+++ my-react-app/src/assetklassen/Fremdwaehrung.jsx
@@ -1,14 +1,17 @@
 import { Outlet, Link } from 'react-router-dom'
-import Navbar from '../fremd_navbar'
+import fremd_navbar from '../fremd_navbar'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 export default function Fremdwaehrung() {
-    
+
 
     return (
         <div>
             <h2>Fremdwaehrung</h2>
-            
+
+            <BrowserRouter>
                 <fremd_navbar />
+            </BrowserRouter>
 
             <Outlet />
         </div>
