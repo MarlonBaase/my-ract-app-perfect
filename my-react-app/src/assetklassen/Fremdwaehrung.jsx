92d6bde9fb7e7aad9032c2baf4e2b93e7e574ef6
MarlonBaase <baase.marlon28@gmail.com>
2026-08-22 14:36:38 +0200

    Version 1.731 - 2026-08-22 14:36:38

--- my-react-app/src/assetklassen/Fremdwaehrung.jsx
+++ my-react-app/src/assetklassen/Fremdwaehrung.jsx
@@ -1,11 +1,18 @@
 import { Outlet, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
 
 
 export default function Fremdwaehrung() {
-  return (
-    <div>
-      <h2>Fremdwaehrung</h2>
-      <Outlet />
-    </div>
-  )
+    return (
+        <div>
+            <h2>Fremdwaehrung</h2>
+
+            <Routes>
+                <Route path="fremdwaehrung_Stammdaten" element={<fremdwaehrung_Stammdaten darkMode={darkMode} />} />
+                <Route path="fremdwaehrung_Konto" element={<fremdwaehrung_Konto darkMode={darkMode} />} />
+            </Routes>
+
+            <Outlet />
+        </div>
+    )
 }
