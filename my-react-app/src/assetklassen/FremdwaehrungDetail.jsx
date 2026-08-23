61427d1755da96e9ef5c2313051855c4890cdf50
MarlonBaase <baase.marlon28@gmail.com>
2026-08-23 12:54:56 +0200

    Version 1.773 - 2026-08-23 12:54:56

--- my-react-app/src/assetklassen/FremdwaehrungDetail.jsx
+++ my-react-app/src/assetklassen/FremdwaehrungDetail.jsx
@@ -1,3 +1,16 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';#
+import { Outlet, Link } from 'react-router-dom'
+
 
 const { code } = useParams(); // 'code' enthält jetzt z. B. "USD"
+
+
+export default function FremdwaehrungDetail() {
+  return (
+    <div>
+      <h2>Detail</h2>
+      
+      <Outlet />
+    </div>
+  )
+}
