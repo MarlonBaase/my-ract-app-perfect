1a6fede937c85edf0809ae8e0248c26ab77d0a5e
MarlonBaase <baase.marlon28@gmail.com>
2026-08-23 10:15:50 +0200

    Version 1.754 - 2026-08-23 10:15:50

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -98,7 +98,10 @@ function App() {
                 <Route path="girokonto" element={<Girokonto darkMode={darkMode} />} />
                 <Route path="tagesgeld" element={<Tagesgeld darkMode={darkMode} />} />
                 <Route path="festgeld" element={<Festgeld darkMode={darkMode} />} />
-                <Route path="fremdwaehrung" element={<Fremdwaehrung darkMode={darkMode} />} />
+                <Route path="fremdwaehrung" element={<Fremdwaehrung darkMode={darkMode} />}>
+                  <Route path="fremdwaehrung_stammdaten" element={<Fremdwaehrung_stammdaten darkMode={darkMode} />} />
+                  <Route path="fremdwaehrung_konto" element={<Fremdwaehrung_konto darkMode={darkMode} />} />
+                </Route>
               </Route>
               <Route path='wd' element={<Wd darkMode={darkMode} />} />
             </Route>
