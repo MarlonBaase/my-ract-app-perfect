0d1613569fb4030698baad6a5b39fd26aee313f5
MarlonBaase <baase.marlon28@gmail.com>
2026-08-23 09:35:41 +0200

    Version 1.744 - 2026-08-23 09:35:41

--- my-react-app/src/fremd_navbar.jsx
+++ my-react-app/src/fremd_navbar.jsx
@@ -4,12 +4,12 @@ import { useState } from 'react'
 const navStruktur = [
   {
     label: 'Währungsstammdaten',
-    path: '/assetklassen/lf/fremdwaehrung_stammaten',
+    path: '/assetklassen/lf/fremdwaehrung/fremdwaehrung_stammaten.jsx',
     unterseiten: []
   },
   {
     label: 'Fremdwährungskonto',
-    path: '/assetklassen/lf/fremdwaehrung_konto',
+    path: '/assetklassen/lf/fremdwaehrung/fremdwaehrung_konto',
     unterseiten: []
   },
   
