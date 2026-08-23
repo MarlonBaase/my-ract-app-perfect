b32f77b5210595669e63b852d1102f0850969b0d
MarlonBaase <baase.marlon28@gmail.com>
2026-08-23 10:33:13 +0200

    Version 1.756 - 2026-08-23 10:33:12

--- my-react-app/src/assetklassen/Fremdwaehrung_stammdaten.jsx
+++ my-react-app/src/assetklassen/Fremdwaehrung_stammdaten.jsx
@@ -1,12 +1,42 @@
 import React, { useState } from 'react';
+import { useEffect, useState, useContext, use } from "react";
+import { supabase } from "../supabase";
+import { handleApiError } from "../utils/errorHandler";
 
 export default function Fremdwaehrung_stammdaten() {
+
+  const [listeWaehrung, setListeWaehrung] = useState("")
+
+  
+  const ladeWaehrungen = async () => {
+          const { data: { user } } = await supabase.auth.getUser()
+          const { data, error } = await supabase
+              .from("waehrungsstammdaten")
+              .select(`waehrung_code, name, symbol`)
+              .eq("asset.benutzer_id", user.id)
+              .order('asset_name', { referencedTable: 'asset', ascending: true });
+  
+          if (handleApiError(error, "Festgeld laden")) return;
+          if (data) setListeWaehrung(data)
+      }
+  
   const [searchTerm, setSearchTerm] = useState('');
-  const items = ['Apfel', 'Banane', 'Kirsche', 'Dattel'];
+  const items = listeWaehrung;
 
   const filteredItems = items.filter(item =>
     item.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  useEffect(() => {
+          const init = async () => {
+              try {
+                  await ladeWaehrungen()
+              } catch (err) {
+                  console.error("Fehler in init:", err);
+              }
+          };
+          init();
+      }, []);
 
   return (
     <div>
