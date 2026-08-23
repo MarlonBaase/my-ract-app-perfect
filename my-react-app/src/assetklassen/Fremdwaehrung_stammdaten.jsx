17dd855db277429784cd2055452f08d340ee0837
MarlonBaase <baase.marlon28@gmail.com>
2026-08-23 10:34:01 +0200

    Version 1.757 - 2026-08-23 10:34:01

--- my-react-app/src/assetklassen/Fremdwaehrung_stammdaten.jsx
+++ my-react-app/src/assetklassen/Fremdwaehrung_stammdaten.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
 import { useEffect, useState, useContext, use } from "react";
 import { supabase } from "../supabase";
 import { handleApiError } from "../utils/errorHandler";
+import { SettingsContext } from '../SettingsContext';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { data } from "react-router-dom";
 
 export default function Fremdwaehrung_stammdaten() {
 
