73f14e3c5aee5e06173ba8876bddb574e8a499b9
MarlonBaase <baase.marlon28@gmail.com>
2026-09-05 10:31:51 +0200

    Staging Build v1.837 - 2026-09-05 10:31:51

--- my-react-app/src/supabase.js
+++ my-react-app/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://zfcpkpmtrsqiuxvkhtbd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmY3BrcG10cnNxaXV4dmtodGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODUwODYsImV4cCI6MjA5NjM2MTA4Nn0.ZjPnnx_ubQwJGS32PMi5hmkjoHmOopjBG38prWIuJHk'
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
